746ffd2341297bc348f6dd10aea3bf852d4efd65
VadimArtish <123artisch.vadim123262003@mail.ru>
2023-04-15 17:39:24 +0300

    add entities

---
+++ src/main/java/chat/com/entity/Message.java
@@ -0,0 +1,35 @@
+package chat.com.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "message_id")
+    private UUID messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String text;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+}
